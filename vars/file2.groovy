d02f715aea7e701364afef8fec16add9b2891ffb
manoj0552 <manoj0552@gmail.com>
2018-07-17 21:11:43 -0500

    Update file2.groovy

--- vars/file2.groovy
+++ vars/file2.groovy
@@ -1,11 +1,12 @@
 
-def call (body) {
-  def config = [:]
-  body resloveStrategy=Closer.DELEGATE_FIRST
-  body.delegate = config
-  body()
-  
-  node{
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
     sh "echo ${config.projectName}" 
   }
   
