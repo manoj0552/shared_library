43b74f9f23f5c0f8ef20a0ba6c29379812777d94
manoj <manoj0552@gmail.com>
2018-07-18 20:11:43 -0500

    own code

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,10 +1,10 @@
 
 def call(){
-      checkout scm[
+      checkout scm: [
       $class:'GitSCM',
       branches:[[name:'*/master']],
       userRemoteConfigs:[[credetialsId:'fbd8b275-22c9-4cb1-8645-7f9d51e92fee',
-      Url:'https://github.com/manoj0552/simple-maven-project-with-tests.git']]
+      url:'https://github.com/manoj0552/simple-maven-project-with-tests.git']]
       ]
 
 }
