8289931e54591b1fcc7f923f45fb729fbb4223cf
manoj <manoj0552@gmail.com>
2018-07-18 21:03:45 -0500

    added s3 upload fucntion

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -4,7 +4,7 @@ def call(){
       $class:'GitSCM',
       branches:[[name:'*/master']],
       userRemoteConfigs:[[credetialsId:'fbd8b275-22c9-4cb1-8645-7f9d51e92fee',
-      url:'https://github.com/manoj0552/simple-maven-project-with-tests.git']]
+      url:'https://github.com/manoj0552/spring4-mvc-example.git']]
       ]
 
 }
